76c66e6a6a0f64f398ffc5d1627bd1a2e5111e20
Mark Nottingham <mnot@mnot.net>
2012-05-10 14:17:41 +1000

    add install_requires for simplejson

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ setup(
   packages = ['uritemplate'],
   provides = ['uritemplate'],
   long_description=open("README.rst").read(),
+  install_requires = ['simplejson >= 2.5.0'],
   classifiers = [
     'Development Status :: 4 - Beta',
     'Intended Audience :: Developers',
